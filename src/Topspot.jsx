abb57597f227b055a718a92fc1a9df29700ceb97
William Kevin Broxton <90276407+wkbroxton@users.noreply.github.com>
2022-05-19 22:12:33 -0700

    done with functionality & minor css

--- src/Topspot.jsx
+++ src/Topspot.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
 export default props => (
-<div className='well'>
-	<h4>{props.name}</h4>
-	<p>{props.description}</p>
-</div>
+    <div className="spots well">
+        <a href={`https://maps.google.com/?q=${props.location[0]},${props.location[1]}`}  
+        // onClick={Hello}
+        >
+            <h4>{props.name}</h4>
+            <p>{props.description}</p>
+        </a>
+    </div>
 );
+{/* <Link to={{ pathname: "https://maps.google.com/?q={this.props.location[0]},{this.props.location[1]" }} target="_blank" > */}
+    {/* </Link> */}
+
+    const Hello = () => {
+        alert("Hello")
+    }
--- src/app.jsx
+++ src/app.jsx
@@ -18,27 +18,33 @@ export default class App extends Component {
       .then(topspots => this.setState({ topspots }));
 	}
 
+  handleClick(){
+
+  }
 
   render() {
     return (
       <div className='App'>
-        <div className='container border border-primary"'>
-          <h2>San Diego Top Spots</h2>
-          <h5>A list of the top 30 places to see in San Diego, California.</h5>
+        <div className='container whiteout'>
+          <div className='border border-primary ttop'>
+            <h2>San Diego Top Spots</h2>
+            <h5>A list of the top 30 places to see in San Diego, California.</h5>
+            <hr id="hr1"/>
+          </div>
+          <hr id="hr2"/>
         </div>
-        <hr/>
-        <pre>
+        <div>
           { 
             this.state.topspots.map(topspot => (
               <TopSpot
-                key={topspot.id}
-                name={topspot.name}
-                description={topspot.description}
-                location={topspot.location} 
+              key={topspot.id}
+              name={topspot.name}
+              description={topspot.description}
+              location={topspot.location}
               />
-            )
-          )}
-        </pre>
+              )
+              )}
+        </div>
       </div>
     );
   }
